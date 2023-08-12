8008f8fd494ef9807d2746929e41a38d6e54d358
HimanshuGupta673 <himanshuu673@gmail.com>
2023-08-12 16:25:04 +0530

    Done with plans page

--- screens/PlanScreen.js
+++ screens/PlanScreen.js
@@ -5,8 +5,8 @@ import {
     SafeAreaView,
     Pressable,
     ScrollView
-  } from "react-native";
-import React from 'react'
+} from "react-native";
+import React, { useState } from 'react'
 import { Feather } from "@expo/vector-icons";
 import { Fontisto } from "@expo/vector-icons";
 import { Entypo } from "@expo/vector-icons";
@@ -15,152 +15,189 @@ import plans from "../data/plans";
 
 const PlanScreen = () => {
     const data = plans
+    const [selected, setSelected] = useState([]);
+    const [price, setPrice] = useState();
     return (
-        <ScrollView style={{ marginTop: 50 }}>
-        <View style={{ padding: 10 }}>
-            <Text style={{ fontSize: 18, fontWeight: "600" }}>
-                Choose the plan that is right for you
-            </Text>
+        <>
+            <ScrollView style={{ marginTop: 50 }}>
+                <View style={{ padding: 10 }}>
+                    <Text style={{ fontSize: 18, fontWeight: "600" }}>
+                        Choose the plan that is right for you
+                    </Text>
 
-            <View
-                style={{
-                    flexDirection: "row",
-                    alignItems: "center",
-                    marginTop: 10,
-                }}
-            >
-                <Feather name="check" size={24} color="#E50914" />
-                <Text style={{ marginLeft: 6, fontSize: 16, fontWeight: "600" }}>
-                    Watch all you want Ad free
-                </Text>
-            </View>
-
-            <View
-                style={{ flexDirection: "row", alignItems: "center", marginTop: 3 }}
-            >
-                <Feather name="check" size={24} color="#E50914" />
-                <Text style={{ marginLeft: 6, fontSize: 16, fontWeight: "600" }}>
-                    Recommendations just for you
-                </Text>
-            </View>
-
-            <View style={{ flexDirection: "row", alignItems: "center" }}>
-                <Feather name="check" size={24} color="#E50914" />
-                <Text
-                    style={{
-                        marginLeft: 6,
-                        fontSize: 16,
-                        fontWeight: "600",
-                        marginTop: 3,
-                    }}
-                >
-                    Cancel your Plan anytime
-                </Text>
-            </View>
-
-            <View style={{ marginTop: 20 }} />
-            {data.map((item, index) => (
-                <Pressable
-                    
-                    style={
-                     
-                            {
-                                height: 170,
-                                borderRadius: 7,
-                                borderColor: "#E50914",
-                                borderWidth: 2,
-                                padding: 15,
-                                margin: 10,
-                            }
-                    
-                    }
-                    key={index}
-                >
                     <View
                         style={{
                             flexDirection: "row",
-                            justifyContent: "space-between",
+                            alignItems: "center",
+                            marginTop: 10,
                         }}
                     >
-                        <View
+                        <Feather name="check" size={24} color="#E50914" />
+                        <Text style={{ marginLeft: 6, fontSize: 16, fontWeight: "600" }}>
+                            Watch all you want Ad free
+                        </Text>
+                    </View>
+
+                    <View
+                        style={{ flexDirection: "row", alignItems: "center", marginTop: 3 }}
+                    >
+                        <Feather name="check" size={24} color="#E50914" />
+                        <Text style={{ marginLeft: 6, fontSize: 16, fontWeight: "600" }}>
+                            Recommendations just for you
+                        </Text>
+                    </View>
+
+                    <View style={{ flexDirection: "row", alignItems: "center" }}>
+                        <Feather name="check" size={24} color="#E50914" />
+                        <Text
                             style={{
-                                backgroundColor: "#E50914",
-                                padding: 10,
-                                width: 120,
-                                borderRadius: 7,
+                                marginLeft: 6,
+                                fontSize: 16,
+                                fontWeight: "600",
+                                marginTop: 3,
                             }}
                         >
-                            <Text
+                            Cancel your Plan anytime
+                        </Text>
+                    </View>
+
+                    <View style={{ marginTop: 20 }} />
+                    {data.map((item, index) => (
+                        <Pressable
+                            onPress={() => {
+                                setSelected(item.name);
+                                setPrice(item.price);
+                            }}
+                            style={
+                                selected.includes(item.name)
+                                    ? {
+                                        height: 170,
+                                        borderRadius: 7,
+                                        borderColor: "#E50914",
+                                        borderWidth: 2,
+                                        padding: 15,
+                                        margin: 10,
+                                    }
+                                    : {
+                                        height: 170,
+                                        borderRadius: 7,
+                                        borderColor: "#E50914",
+                                        borderWidth: 0.5,
+                                        padding: 15,
+                                        margin: 10,
+                                    }
+                            }
+                            key={index}
+                        >
+                            <View
                                 style={{
-                                    textAlign: "center",
-                                    color: "white",
-                                    fontSize: 16,
-                                    fontWeight: "600",
+                                    flexDirection: "row",
+                                    justifyContent: "space-between",
                                 }}
                             >
-                                {item.name}
-                            </Text>
-                        </View>
+                                <View
+                                    style={{
+                                        backgroundColor: "#E50914",
+                                        padding: 10,
+                                        width: 120,
+                                        borderRadius: 7,
+                                    }}
+                                >
+                                    <Text
+                                        style={{
+                                            textAlign: "center",
+                                            color: "white",
+                                            fontSize: 16,
+                                            fontWeight: "600",
+                                        }}
+                                    >
+                                        {item.name}
+                                    </Text>
+                                </View>
 
-                        <Text style={{ fontSize: 18, fontWeight: "600" }}>
-                            Price: ₹{item.price}
-                        </Text>
-                    </View>
+                                <Text style={{ fontSize: 18, fontWeight: "600" }}>
+                                    Price: ₹{item.price}
+                                </Text>
+                            </View>
 
-                    <View
-                        style={{
-                            marginTop: 15,
-                            flexDirection: "row",
-                            alignItems: "center",
-                            justifyContent: "space-between",
-                        }}
-                    >
-                        <View>
-                            <Text
-                                style={{ color: "gray", fontSize: 15, fontWeight: "500" }}
+                            <View
+                                style={{
+                                    marginTop: 15,
+                                    flexDirection: "row",
+                                    alignItems: "center",
+                                    justifyContent: "space-between",
+                                }}
                             >
-                                video Quality : {item.videoQuality}
-                            </Text>
-                            <Text
-                                style={{ fontSize: 16, fontWeight: "500", marginTop: 3 }}
-                            >
-                                Resolution : {item.resolution}
-                            </Text>
-                        </View>
-                        <Fontisto
-                            style={{ marginRight: 6 }}
-                            name="netflix"
-                            size={28}
-                            color="black"
-                        />
-                    </View>
-
-                    <View
-                        style={{
-                            marginTop: 10,
-                            flexDirection: "row",
-                            alignItems: "center",
-                        }}
-                    >
-                        <Text style={{ fontSize: 16 }}>
-                            Devices You can watch On :{" "}
-                        </Text>
-                        <View style={{ flexDirection: "row", alignItems: "center" }}>
-                            {item.devices.map((device,index) => (
-                                <Entypo
-                                    style={{ marginRight: 3 }}
-                                    name={device.name}
-                                    size={21}
-                                    color="#E50914"
-                                    key={index}
+                                <View>
+                                    <Text
+                                        style={{ color: "gray", fontSize: 15, fontWeight: "500" }}
+                                    >
+                                        video Quality : {item.videoQuality}
+                                    </Text>
+                                    <Text
+                                        style={{ fontSize: 16, fontWeight: "500", marginTop: 3 }}
+                                    >
+                                        Resolution : {item.resolution}
+                                    </Text>
+                                </View>
+                                <Fontisto
+                                    style={{ marginRight: 6 }}
+                                    name="netflix"
+                                    size={28}
+                                    color="black"
                                 />
-                            ))}
-                        </View>
+                            </View>
+
+                            <View
+                                style={{
+                                    marginTop: 10,
+                                    flexDirection: "row",
+                                    alignItems: "center",
+                                }}
+                            >
+                                <Text style={{ fontSize: 16 }}>
+                                    Devices You can watch On :{" "}
+                                </Text>
+                                <View style={{ flexDirection: "row", alignItems: "center" }}>
+                                    {item.devices.map((device, index) => (
+                                        <Entypo
+                                            style={{ marginRight: 3 }}
+                                            name={device.name}
+                                            size={21}
+                                            color="#E50914"
+                                            key={index}
+                                        />
+                                    ))}
+                                </View>
+                            </View>
+                        </Pressable>
+                    ))}
+                </View>
+            </ScrollView>
+
+            {selected.length > 0 ? (
+                <Pressable
+                    style={{
+                        backgroundColor: "#E50914",
+                        padding: 10,
+                        flexDirection: "row",
+                        alignItems: "center",
+                        justifyContent: "space-between",
+                        height: 55,
+                    }}
+                >
+                    <View>
+                        <Text style={{ color: "white", fontSize: 17, fontWeight: "600" }}>Selected Plan {selected}</Text>
                     </View>
+
+                    <Pressable>
+                        <Text style={{ fontSize: 17, fontWeight: "bold", color: "white" }}>PAY ₹{price}</Text>
+                    </Pressable>
                 </Pressable>
-            ))}
-        </View>
-        </ScrollView>
+            ) : (
+                null
+            )}
+        </>
     )
 }
 
